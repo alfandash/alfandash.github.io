506f9120eaecaace096673a37e62ac4d57ad8191
Alfan D A <cumiasem91@gmail.com>
2017-07-19 08:26:05 +0700

    update

--- week2/Tugasweek2:MelakukanLoopingMenggunakanFor.js
+++
@@ -1,8 +0,0 @@
-for (var looping=1 ; looping <= 20 ; looping++ ) {
-  console.log(looping + ' - i love coding');
-  if (looping == 20) {
-    for (var looping_2=20; looping_2 > 0; looping_2--) {
-      console.log(looping_2 + '- I will become fullstack developer');
-    }
-  }
-}
